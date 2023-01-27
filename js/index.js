48f0bdea178685bf780e0ba31de8d3a7ca8dbbb3
alexisddang <60058312+alexisddang@users.noreply.github.com>
2023-01-27 15:35:15 -0500

    scrollspy navbar attempt 2 :)

--- js/index.js
+++ js/index.js
@@ -1,60 +1,55 @@
+//Smooth Scroll//
 // https://codepen.io/ifabre/pen/qBZarQ
 
+$(".homeButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#homePage").offset().top      
+    },100);
+ return false;
+});
 
-// //
+$(".projectButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projectPage").offset().top
+    },100);
+ return false;
+});
 
-// $(".homeButton").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#homePage").offset().top      
-//     },100);
-//  return false;
-// });
+$(".aboutButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#aboutPage").offset().top
+    },100);
+ return false;
+});
 
-// $(".projectButton").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#projectPage").offset().top
-//     },100);
-//  return false;
-// });
+$(".contactButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contactPage").offset().top
+    },100);
+ return false;
+});
 
-// $(".aboutButton").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#aboutPage").offset().top
-//     },100);
-//  return false;
-// });
+//Scrollspy//
+// https://codepen.io/acarlie/pen/LKmORw?editors=0110
+$(document).ready(function() {
+    var sectionIds = $("a.navButton");
 
-// $(".contactButton").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#contactPage").offset().top
-//     },100);
-//  return false;
-// });
+    $(document).scroll(function() {
+        sectionIds.each(function() {
+            var container = $(this).attr("href");
+            var containerOffset = $(container).offset().top;
+            var containerHeight = $(container).outerHeight();
+            var containerBottom = containerOffset + containerHeight;
+            var scrollPosition = $(document).scrollTop();
 
-// //
-$(".homeButton").addClass("active");
+            if(scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20){
+                $(this).addClass('active');
+            } else{
+                $(this).removeClass('active');
+            }
+        });
+    });
+});
 
-  $('#homePage').waypoint(function() {
-    $(".topNav h3").children().removeClass("active");
-    $(".homeButton").addClass("active");
-  }, { offset: -1 });
 
-$('#projectPage').waypoint(function() {
-
-    $(".topNav h3").children().removeClass("active");
-    $(".projectButton").addClass("active");
-    
-  }, { offset: 0 });
-  
-  $('#aboutPage').waypoint(function() {
-    $(".topNav h3").children().removeClass("active");
-    $(".aboutButton").addClass("active");
-  }, { offset: 0 });
-  
-  $('#contactPage').waypoint(function() {
-    $(".topNav h3").children().removeClass("active");
-    $(".contactButton").addClass("active");
-  }, { offset: 0 });
-
-  ////
 

9c4a0f81519d3a61bad13e381b3088997243a0d0
alexisddang <60058312+alexisddang@users.noreply.github.com>
2023-03-26 18:18:49 -0700

    affinity carousel

--- js/index.js
+++ js/index.js
@@ -87,3 +87,42 @@ $(function() {
         backgroundColor: "#ED33B9" //to pink
     },1200);
 });
+
+
+
+
+// Affinity Diagram Modals
+
+var modal1 = document.getElementById("modal1");
+var modal2 = document.getElementById("modal2");
+
+var img1 = document.getElementById("affinity1Img");
+var modalImg1 = document.getElementById("img01");
+var captionText1 = document.getElementById("caption1");
+var img2 = document.getElementById("affinity2Img");
+var modalImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption2");
+
+img1.onclick = function() {
+    modal1.style.display = "block";
+    modalImg1.src = this.src;
+    captionText1.innerHTML = this.alt;
+}
+
+img2.onclick = function() {
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+    captionText2.innerHTML = this.alt;
+}
+
+var span1 = document.getElementsByClassName("close1")[0];
+
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+var span2 = document.getElementsByClassName("close2")[0];
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
